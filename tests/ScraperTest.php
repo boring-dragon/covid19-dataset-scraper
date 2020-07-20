c43e079870268726f60a50ab3a5f0a989dc2f207
Mohamed jinas <j@live.mv>
2020-07-21 04:16:38 +0500

    added tests

---
+++ tests/ScraperTest.php
@@ -0,0 +1,28 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Jinas\Covid19Scraper\Scraper;
+
+class ScraperTest extends TestCase 
+{
+     /** @test */
+    public function scraper_returns_an_array_of_table_data()
+    {
+        $data = (new Scraper)->GetTableData();
+
+        $this->assertIsArray($data);
+
+        $this->assertArrayHasKey("case", $data[0]);
+        $this->assertArrayHasKey("age", $data[0]);
+        $this->assertArrayHasKey("gender", $data[0]);
+        $this->assertArrayHasKey("nationality", $data[0]);
+        $this->assertArrayHasKey("condition", $data[0]);
+        $this->assertArrayHasKey("transmission", $data[0]);
+        $this->assertArrayHasKey("cluster", $data[0]);
+        $this->assertArrayHasKey("confirmed", $data[0]);
+        $this->assertArrayHasKey("recovered", $data[0]);
+        $this->assertArrayHasKey("discharged", $data[0]);
+        $this->assertArrayHasKey("deceased", $data[0]);
+
+        $this->assertEquals("MAV0001", array_reverse($data)[0]["case"]);
+    }
+}
